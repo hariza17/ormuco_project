20da10c489121e878f9ded3e60018bdc548d4771
hariza17 <hclareth7@gmail.com>
2017-01-15 23:21:37 -0500

    ok

--- ormuco_project/urls.py
+++ ormuco_project/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import include, url
+from django.http import HttpResponseRedirect
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$', include('ormuco.urls', namespace="ormuco")),
-    url(r'^admin/', admin.site.urls),
+	url(r'^$', lambda r: HttpResponseRedirect('ormuco/')),
+    url(r'^ormuco/', include('ormuco.urls', namespace="ormuco")),
 ]
