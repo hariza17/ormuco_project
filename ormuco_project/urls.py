25fedc36ea38446eb4de33bafe61c551cce559c8
hariza17 <hclareth7@gmail.com>
2017-01-15 23:02:12 -0500

    3

--- ormuco_project/urls.py
+++ ormuco_project/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^ormuco/', include('ormuco.urls', namespace="ormuco")),
+    url(r'^/', include('ormuco.urls', namespace="ormuco")),
     url(r'^admin/', admin.site.urls),
 ]
